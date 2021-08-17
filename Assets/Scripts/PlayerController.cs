9ed61c58ac615080e057930f42dbfaf900c87c56
Jet <j3tmarch@gmail.com>
2021-08-17 18:01:54 +0300

    Lesson 3.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,28 @@ public class PlayerController : MonoBehaviour
 
     private bool isGrounded = true;
     private Rigidbody playerRb;
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isGameOver)
+        {
+            return;
+        }
+
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -36,6 +44,8 @@ public class PlayerController : MonoBehaviour
         {
             isGameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
